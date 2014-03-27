c03a406133bb4ee1e995d516a32ceea5e578d5eb
xgdgsc <xgdgsc@gmail.com>
2014-03-27 16:59:56 +0800

    modified:   makeR.sh

--- src/R/makeR.sh
+++ src/R/makeR.sh
@@ -3,6 +3,7 @@
 # export PKG_CXXFLAGS=`Rscript -e "Rcpp:::CxxFlags()"`
 # R CMD SHLIB -lgomp -o Rsomoclu.so Rsomoclu.cpp somocluWrap.cpp somoclu.o denseCpuKernels.o io.o sparseCpuKernels.o training.o mapDistanceFunctions.o
 #rm -rf ./build
+mkdir data
 cp ../../autogen.sh ./src
 cp -r ../../m4 ./src
 cp ../../configure.ac ./src
@@ -12,3 +13,4 @@ cp ../../src/*.cpp ./src/src
 cp ../../src/*.cu ./src/src
 cp ../../src/Makefile.in ./src/src
 cp ../../data/rgbs.txt ./data/
+#gzip ./data/rgbs.txt
