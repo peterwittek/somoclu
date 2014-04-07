77b5f59572c0c74cc354b2a59bdf36160cb45d80
xgdgsc <xgdgsc@gmail.com>
2014-04-07 14:11:27 +0800

    modified:   Rscripts/makeR.sh

--- src/Rscripts/makeR.sh
+++ src/Rscripts/makeR.sh
@@ -5,13 +5,9 @@
 #rm -rf ./build
 cd ../R
 mkdir data
-cp ../../autogen.sh ./src
-cp -r ../../m4 ./src
-cp ../../configure.ac ./src
-cp ../../Makefile.in ./src
-cp ../../src/*.h ./src/src
-cp ../../src/*.cpp ./src/src
-cp ../../src/*.cu ./src/src
-cp ../../src/Makefile.in ./src/src
+cp ../../src/*.h ./src/
+cp ../../src/mapDistanceFunctions.cpp ./src/
+cp ../../src/trainOneEpoch.cpp ./src/
+cp ../../src/uMatrix.cpp ./src/
 cp ../../data/rgbs.txt ./data/
 gzip ./data/rgbs.txt
