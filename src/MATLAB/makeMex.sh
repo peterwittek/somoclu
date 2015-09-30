8d996deb1cd49e527b74c21ab51a2f1f72cc8496
Peter Wittek <peterwittek@users.noreply.github.com>
2015-09-30 11:25:09 +0200

    MATLAB compile scripts integrated in Makefile and configure script

--- src/MATLAB/makeMex.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ -z "$MATLAB_ROOT" ]
-     then MATLAB_ROOT="/usr/local/MATLAB/R2013a/"
-fi
-MEX_BIN="$MATLAB_ROOT/bin/mex"
-$MEX_BIN -I../ MexSomoclu.cpp ../denseCpuKernels.o ../io.o ../sparseCpuKernels.o ../training.o ../mapDistanceFunctions.o ../uMatrix.o -lgomp
