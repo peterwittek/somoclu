7dea4d4d63b50c76b9aad499698111c4a133949f
xgdgsc <xgdgsc@gmail.com>
2017-04-20 21:10:17 +0800

    fix an R-devel note

---
+++ src/R/src/Rsomoclu_init.c
@@ -0,0 +1,22 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME:
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .Call calls */
+extern SEXP Rtrain(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+
+static const R_CallMethodDef CallEntries[] = {
+    {"Rtrain", (DL_FUNC) &Rtrain, 17},
+    {NULL, NULL, 0}
+};
+
+void R_init_Rsomoclu(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
