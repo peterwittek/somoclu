3ce9785764417caf6558564c39ef19afe11d3e31
Peter Wittek <peterwittek@users.noreply.github.com>
2018-01-31 08:45:37 -0500

    U-matrix calculations greatly simplified

--- src/uMatrix.cpp
+++ src/uMatrix.cpp
@@ -1,24 +1,5 @@
-
 #include"somoclu.h"
 #include<cmath>
-
-/** Get weight vector from a codebook using x, y index
- * @param codebook - the codebook to save
- * @param som_y - y coordinate of a node in the map
- * @param som_x - x coordinate of a node in the map
- * @param nSomX - dimensions of SOM map in the x direction
- * @param nSomY - dimensions of SOM map in the y direction
- * @param nDimensions - dimensions of a data instance
- * @return the weight vector
- */
-
-float* get_wvec(float *codebook, unsigned int som_y, unsigned int som_x,
-                unsigned int nSomX, unsigned int nDimensions) {
-    float* wvec = new float[nDimensions];
-    for (unsigned int d = 0; d < nDimensions; d++)
-        wvec[d] = codebook[som_y * nSomX * nDimensions + som_x * nDimensions + d]; /// CAUTION: (y,x) order
-    return wvec;
-}
 
 /** Euclidean distance between vec1 and vec2
  * @param vec1
@@ -27,15 +8,10 @@ float* get_wvec(float *codebook, unsigned int som_y, unsigned int som_x,
  * @return distance
  */
 
-float get_distance(const float* vec1, const float* vec2,
-                   unsigned int nDimensions) {
+float get_distance(float* vec1, float* vec2, unsigned int nDimensions) {
     float distance = 0.0f;
-    float x1 = 0.0f;
-    float x2 = 0.0f;
-    for (unsigned int d = 0; d < nDimensions; d++) {
-        x1 = std::min(vec1[d], vec2[d]);
-        x2 = std::max(vec1[d], vec2[d]);
-        distance += std::abs(x1 - x2) * std::abs(x1 - x2);
+    for (unsigned int d = 0; d < nDimensions; ++d) {
+        distance += (vec1[d] - vec2[d]) * (vec1[d] - vec2[d]);
     }
     return sqrt(distance);
 }
@@ -69,11 +45,11 @@ float *calculateUMatrix(float *uMatrix, float *codebook, unsigned int nSomX,
                 float dist = 0.0f;
                 unsigned int nodes_number = 0;
 #ifdef _WIN32
-				for (int som_y2 = 0; som_y2 < nSomY; som_y2++) {
-					for (int som_x2 = 0; som_x2 < nSomX; som_x2++) {
+				for (int som_y2 = 0; som_y2 < nSomY; ++som_y2) {
+					for (int som_x2 = 0; som_x2 < nSomX; ++som_x2) {
 #else
-                for (unsigned int som_y2 = 0; som_y2 < nSomY; som_y2++) {
-                    for (unsigned int som_x2 = 0; som_x2 < nSomX; som_x2++) {
+                for (unsigned int som_y2 = 0; som_y2 < nSomY; ++som_y2) {
+                    for (unsigned int som_x2 = 0; som_x2 < nSomX; ++som_x2) {
 #endif
                         if (som_x1 == som_x2 && som_y1 == som_y2) continue;
                         float tmp = 0.0f;
@@ -94,12 +70,10 @@ float *calculateUMatrix(float *uMatrix, float *codebook, unsigned int nSomX,
                             }
                         }
                         if (tmp <= min_dist) {
-                            nodes_number++;
-                            float* vec1 = get_wvec(codebook, som_y1, som_x1, nSomX, nDimensions);
-                            float* vec2 = get_wvec(codebook, som_y2, som_x2, nSomX, nDimensions);
-                            dist += get_distance(vec1, vec2, nDimensions);
-                            delete [] vec1;
-                            delete [] vec2;
+                            ++nodes_number;
+                            dist += get_distance(codebook + som_y1 * nSomX * nDimensions + som_x1 * nDimensions,
+                                                 codebook + som_y2 * nSomX * nDimensions + som_x2 * nDimensions,
+                                                 nDimensions);
                         }
                     }
                 }
